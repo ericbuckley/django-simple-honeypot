d9a037136d057174b3908a7429cc0bd36141be45
Eric Buckley <eric.buckley@gmail.com>
2010-08-10 20:46:29 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-simple-honeypot',
+      version='1.0',
+      description='Django honeypot field that you can add to your forms to reduce the chance of bot spam',
+      author='Eric Buckley',
+      author_email='eric.buckley@gmail.com',
+      url='http://github.com/ericbuckley/django-simple-honeypot',
+     )
